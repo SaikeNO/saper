1f999c4ef0f0f2c4ae8792b48a966e8353500008
Mateusz Lengiewicz <mateusz.lengiewicz111@gmail.com>
2021-12-17 19:19:34 +0100

    refator: DRY code

--- main.js
+++ main.js
@@ -8,7 +8,19 @@ class Saper {
     this.timer = document.getElementById("timer");
     this.indexInterval = null;
     this.flagCounter = 0;
+    this.btnsElems = [];
+    this.bombsTab = [];
   }
+
+  //helpers
+  randomNumber(number) {
+    return Math.floor(Math.random() * number);
+  }
+
+  getBtn(x, y) {
+    return document.querySelector(`[data-x="${x}"][data-y="${y}"]`);
+  }
+  //end helpers
 
   generateBoard() {
     for (let y = 0; y < this.fields; y++) {
@@ -19,25 +31,14 @@ class Saper {
     }
   }
 
-  showFields() {
-    for (let y = 0; y < this.board.length; y++) {
-      for (let x = 0; x < this.board[y].length; x++) {
-        const elem = document.createElement("div");
-        elem.classList.add("btn");
-        elem.dataset.y = y;
-        elem.dataset.x = x;
-        this.boardElem.appendChild(elem);
-        this.boardElem.style.gridTemplateColumns = `repeat(${this.fields}, 1fr)`;
-      }
-    }
-  }
-
   placeBombs() {
     const tab = [];
     let i = this.bombs;
     while (i > 0) {
-      const row = Math.floor(Math.random() * this.fields);
-      const column = Math.floor(Math.random() * this.fields);
+      const row = this.randomNumber(this.fields);
+      const column = this.randomNumber(this.fields);
+
+      // checking if bomb position doesnt repeat
       if (!tab.includes(`${row}${column}`)) {
         tab.push(`${row}${column}`);
         this.board[column][row] = -1;
@@ -52,16 +53,165 @@ class Saper {
         if (this.board[y][x] !== -1) {
           let number = 0;
 
-          for (let offsetX = -1; offsetX <= 1; offsetX++) {
-            for (let offsetY = -1; offsetY <= 1; offsetY++) {
-              if (this.board[y + offsetY]?.[x + offsetX] === -1) number++;
-            }
-          }
+          const nearbyFields = this.nearbyFields(x, y);
+
+          nearbyFields.forEach((elem) => {
+            if (this.board[elem.y]?.[elem.x] === -1) number++;
+          });
 
           this.board[y][x] = number;
         }
       }
     }
+  }
+
+  showFields() {
+    for (let y = 0; y < this.fields; y++) {
+      for (let x = 0; x < this.fields; x++) {
+        const elem = document.createElement("div");
+        elem.classList.add("btn");
+        elem.dataset.y = y;
+        elem.dataset.x = x;
+        this.boardElem.appendChild(elem);
+
+        // create tab that contains bombs position
+        if (this.board[elem.dataset.y][elem.dataset.x] === -1) {
+          this.bombsTab.push(elem);
+        }
+      }
+    }
+    this.boardElem.style.gridTemplateColumns = `repeat(${this.fields}, 1fr)`;
+
+    // create global tab that contains all btns objects
+    this.btnsElems = [...document.querySelectorAll("#board div")];
+  }
+
+  nearbyFields(x, y) {
+    let tab = [];
+
+    // checking 8 nearby fields
+    for (let offsetX = -1; offsetX <= 1; offsetX++) {
+      for (let offsetY = -1; offsetY <= 1; offsetY++) {
+        const btn = this.getBtn(offsetX + x, offsetY + y);
+
+        if (
+          offsetX + x < this.fields &&
+          offsetY + y < this.fields &&
+          offsetY + y >= 0 &&
+          offsetX + x >= 0 &&
+          !btn?.classList.contains("btn--active") &&
+          !btn?.classList.contains("flag")
+        ) {
+          const field = {
+            element: btn,
+            x: offsetX + x,
+            y: offsetY + y,
+          };
+
+          tab.push(field);
+        }
+      }
+    }
+    return tab;
+  }
+
+  revealFields(nearbyFieldsTab) {
+    nearbyFieldsTab.forEach((field) => {
+      if (this.board[field.y]?.[field.x] === -1) return;
+
+      field.element.classList.add("btn--active");
+      field.element.classList.add(`btn--${this.board[field.y]?.[field.x]}`);
+
+      if (this.board[field.y]?.[field.x] !== 0) {
+        field.element.textContent = this.board[field.y]?.[field.x];
+      } else {
+        const tab = this.nearbyFields(field.x, field.y);
+        this.revealFields(tab);
+      }
+    });
+  }
+
+  checkWin() {
+    const filteredBombsTab = this.bombsTab.filter(
+      (btn) => !btn.classList.contains("flag")
+    );
+    const filteredBtnsTab = this.btnsElems.filter(
+      (btn) => btn.classList.length === 1
+    );
+
+    const contidional =
+      filteredBombsTab.length === 0 &&
+      filteredBtnsTab.length === 0 &&
+      this.flagCounter === this.bombs;
+
+    if (contidional) {
+      const winElem = document.querySelector(".overlay-win");
+
+      //win
+      this.boardElem.classList.add("win");
+      winElem.classList.add("active");
+      clearInterval(this.indexInterval);
+    }
+  }
+
+  rightClick(btn) {
+    if (!btn.classList.contains("btn--active")) {
+      if (btn.classList.contains("flag")) {
+        btn.innerHTML = "";
+        this.flagCounter--;
+      } else {
+        this.flagCounter++;
+        //set flag icon
+        btn.innerHTML = "&#128681;";
+      }
+      btn.classList.toggle("flag");
+    }
+  }
+
+  leftClick(btn) {
+    if (!btn.classList.contains("flag")) {
+      const x = parseInt(btn.dataset.x);
+      const y = parseInt(btn.dataset.y);
+      const boardItem = this.board[y][x];
+      if (boardItem === -1) {
+        // set bomb icon
+        btn.innerHTML = "&#128163;";
+
+        this.defeat();
+      } else if (boardItem === 0) {
+        const nearbyFieldsTab = this.nearbyFields(x, y);
+        this.revealFields(nearbyFieldsTab);
+      } else {
+        btn.textContent = boardItem;
+        btn.classList.add("btn--active");
+        btn.classList.add(`btn--${boardItem}`);
+      }
+    }
+  }
+
+  addListeners() {
+    this.btnsElems.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        this.leftClick(btn);
+        this.checkWin();
+      });
+      btn.addEventListener("contextmenu", (e) => {
+        e.preventDefault();
+        this.rightClick(btn);
+        this.checkWin();
+      });
+    });
+  }
+
+  defeat() {
+    this.boardElem.classList.add("lost");
+    clearInterval(this.indexInterval);
+
+    this.bombsTab.forEach((btn) => {
+      if (!btn.classList.contains("flag")) {
+        btn.innerHTML = "&#128163;";
+      }
+    });
   }
 
   setTime() {
@@ -72,128 +222,6 @@ class Saper {
       this.timer.textContent = time;
       time++;
     }, 1000);
-  }
-
-  defeat(btns) {
-    this.boardElem.classList.add("lost");
-    clearInterval(this.indexInterval);
-
-    for (let y = 0; y < this.fields; y++) {
-      for (let x = 0; x < this.fields; x++) {
-        btns.forEach((btn) => {
-          const x = btn.dataset.x;
-          const y = btn.dataset.y;
-          const boardItem = this.board[y][x];
-          if (boardItem === -1 && !btn.classList.contains("flag")) {
-            btn.innerHTML = "&#128163;";
-          }
-        });
-      }
-    }
-  }
-
-  nearbyFields(x, y) {
-    let tab = [];
-    for (let offsetX = -1; offsetX <= 1; offsetX++) {
-      for (let offsetY = -1; offsetY <= 1; offsetY++) {
-        const btn = document.querySelector(
-          `[data-x="${offsetX + x}"][data-y="${offsetY + y}"]`
-        );
-
-        if (
-          offsetX + x < this.fields &&
-          offsetY + y < this.fields &&
-          offsetY + y >= 0 &&
-          offsetX + x >= 0 &&
-          !btn.classList.contains("btn--active") &&
-          !btn.classList.contains("flag")
-        ) {
-          const tile = {
-            element: btn,
-            x: offsetX + x,
-            y: offsetY + y,
-          };
-          tab.push(tile);
-        }
-      }
-    }
-    return tab;
-  }
-
-  revealFields(nearbyFieldsTab) {
-    nearbyFieldsTab.forEach((field) => {
-      if (this.board[field.y]?.[field.x] === -1) return;
-      field.element.classList.add("btn--active");
-      field.element.classList.add(`btn--${this.board[field.y]?.[field.x]}`);
-      if (this.board[field.y]?.[field.x] !== 0) {
-        field.element.textContent = this.board[field.y]?.[field.x];
-      }
-      if (this.board[field.y]?.[field.x] === 0) {
-        const tab = this.nearbyFields(field.x, field.y);
-        this.revealFields(tab);
-      }
-    });
-  }
-
-  checkWin(btns) {
-    let number = 0;
-    const winElem = document.querySelector(".overlay-win");
-    for (let y = 0; y < this.fields; y++) {
-      for (let x = 0; x < this.fields; x++) {
-        const btn = document.querySelector(`[data-x="${x}"][data-y="${y}"]`);
-        if (this.board[y][x] === -1 && btn.classList.contains("flag")) number++;
-
-        const tab = btns.filter((btn) => btn.classList.length === 1);
-
-        let contidtional =
-          number === this.bombs &&
-          this.flagCounter === this.bombs &&
-          tab.length === 0;
-
-        if (contidtional) {
-          this.boardElem.classList.add("win");
-          winElem.classList.add("active");
-        }
-      }
-    }
-  }
-
-  addListeners() {
-    const btns = [...document.querySelectorAll("#board div")];
-
-    btns.forEach((btn) => {
-      btn.addEventListener("click", () => {
-        if (!btn.classList.contains("flag")) {
-          const x = parseInt(btn.dataset.x);
-          const y = parseInt(btn.dataset.y);
-          const boardItem = this.board[y][x];
-          if (boardItem === -1) {
-            btn.innerHTML = "&#128163;";
-            this.defeat(btns);
-          } else if (boardItem === 0) {
-            const nearbyFieldsTab = this.nearbyFields(x, y);
-            this.revealFields(nearbyFieldsTab);
-          } else {
-            btn.textContent = boardItem;
-            btn.classList.add("btn--active");
-            btn.classList.add(`btn--${boardItem}`);
-          }
-          this.checkWin(btns);
-        }
-      });
-      btn.addEventListener("contextmenu", (e) => {
-        e.preventDefault();
-        if (btn.classList.contains("flag")) {
-          btn.innerHTML = "";
-          this.flagCounter--;
-        } else {
-          this.flagCounter++;
-          btn.innerHTML = "&#128681;";
-        }
-        btn.classList.toggle("flag");
-        this.checkWin(btns);
-      });
-    });
   }
 
   init() {
@@ -224,7 +252,7 @@ let saper;
 
 easyBtn.addEventListener("click", () => {
   if (!board.innerHTML) {
-    saper = new Saper(board, 7, 2);
+    saper = new Saper(board, 9, 2);
     saper.init();
     easyBtn.classList.add("active");
   }
@@ -240,7 +268,7 @@ mediumBtn.addEventListener("click", () => {
 
 hardBtn.addEventListener("click", () => {
   if (!board.innerHTML) {
-    saper = new Saper(board, 17, 20);
+    saper = new Saper(board, 21, 50);
     saper.init();
     hardBtn.classList.add("active");
   }
@@ -255,5 +283,5 @@ closeBtn.addEventListener("click", () => {
   hardBtn.classList.remove("active");
   counter.textContent = 0;
   timer.textContent = 0;
-  saper.destroy();
+  saper?.destroy();
 });
